4a9d7d710078a9552fe4676010094e0ed007a7bf
Aksel Villela <jonhaksel@gmail.com>
2024-09-03 23:32:20 -0600

    tarea4

---
+++ Tarea_4/main.py
@@ -0,0 +1,15 @@
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def main():
+    n = 8
+    print(f"Resultado de la serie de Fibonacci para n = {n} es: {fibonacci(n)}")
+
+
+if __name__ == '__main__':
+    main()
